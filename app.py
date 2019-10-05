b135f2e4c189437dfe654d2220bfdca28e696485
tolumide <tolumideshopein@gmail.com>
2019-10-05 11:14:34 +0100

    Add edit property for todo list

--- app.py
+++ app.py
@@ -16,15 +16,32 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mac@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class Todo(db.Model):
     __tablename__ = 'todos'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
-    
+
     def __repr__(self):
         return f'<Todo description={self.description} id={self.id}'
+
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed(todo_id):
+    try:
+        completed = request.get_json(['completed'])
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        print('it got here')
+
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+        # return(redirect(url_for('/index')))
 
 
 @app.route('/todos/create', methods=['POST'])
@@ -50,7 +67,6 @@ def create_todo():
         return jsonify(body)
 
 
-
 @app.route('/')
 def index():
-	return render_template('index.html', data = Todo.query.all())
+    return render_template('index.html', data=Todo.query.all())
